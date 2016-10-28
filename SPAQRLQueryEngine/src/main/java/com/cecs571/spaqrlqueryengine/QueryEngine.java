a64704d1f83b42fa9a240cf4d7ece7caae4280ea
timphan212 <timphan212@gmail.com>
2016-10-28 00:55:12 -0700

    project setup with jena

---
+++ SPAQRLQueryEngine/src/main/java/com/cecs571/spaqrlqueryengine/QueryEngine.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cecs571.spaqrlqueryengine;
+
+/**
+ *
+ * @author Tim
+ */
+public class QueryEngine {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
