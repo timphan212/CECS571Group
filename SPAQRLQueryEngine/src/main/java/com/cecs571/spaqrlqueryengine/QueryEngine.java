b682404a88a927ed64b8ca5abd7227b4dd5cf157
Jay Patel <pateljay143@gmail.com>
2016-11-06 11:11:16 -0800

    Package and Imports declaration

---
+++ SPAQRLQueryEngine/src/main/java/com/cecs571/spaqrlqueryengine/QueryEngine.java
@@ -0,0 +1,10 @@
+package com.cecs571.spaqrlqueryengine;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.util.FileManager;
