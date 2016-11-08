701c2a0ce0d9d25cd56d584dd35b4aba70ab204a
pateljay143 <pateljay143@gmail.com>
2016-11-08 14:30:06 -0800

    Class to generate output in HTML

---
+++ SPAQRLQueryEngine/src/main/java/com/cecs571/spaqrlqueryengine/HTMLFactory.java
@@ -0,0 +1,18 @@
+package com.cecs571.spaqrlqueryengine;
+
+import com.webfirmframework.wffweb.tag.html.Body;
+import com.webfirmframework.wffweb.tag.html.Html;
+import com.webfirmframework.wffweb.tag.html.tables.Table;
+import com.webfirmframework.wffweb.tag.html.tables.Td;
+import com.webfirmframework.wffweb.tag.html.tables.Th;
+import com.webfirmframework.wffweb.tag.html.tables.Tr;
+import com.webfirmframework.wffweb.tag.htmlwff.NoTag;
+public class HTMLFactory {
+    private final Html html;
+    private final Body body;
+    public HTMLFactory() {
+        html = new Html(null);
+        html.setPrependDocType(true);
+        body = new Body(html);
+    }
+}
